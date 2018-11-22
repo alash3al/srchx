a65d37e4fbe0b1230f1576c1d2d3fb45126ff7c5
Mohamed Al Ashaal <m7medalash3al@gmail.com>
2018-11-22 12:15:39 +0200

    fixed the struct tags

--- search_result.go
+++ search_result.go
@@ -3,6 +3,6 @@ package main
 // SearchResult - a search result
 type SearchResult struct {
 	Totals uint64                   `json:"totals"`
-	Time   int64                    `time:"time"`
-	Docs   []map[string]interface{} `docs:"docs"`
+	Time   int64                    `json:"time"`
+	Docs   []map[string]interface{} `json:"docs"`
 }
